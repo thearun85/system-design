e4bfe4a5e0a33ccd0a9c81a43c5c7c9a99e1b874
Arun Raghunath <arun2aruns@gmail.com>
2025-11-26 17:11:53 +0000

    A simple Round robin with health check

--- loadbalancer/README.md
+++ loadbalancer/README.md
@@ -4,7 +4,7 @@ This project is to demonstrate loadbalancers, the various categories and other a
 ## v1 - 
 a service created which can be run as multiple instances on different ports.
 
-## v2 - a round robin load balancer created, which serves as the front end and distributes traffic to the backends.
+## v2 - a round robin load balancer created, which serves as the front end and distributes traffic to the backends. There is no real health check. If the url's are down, the request will still be forwarded to them which responds with a service error
 
 ## v3 - dockerized the project. 
 1. Dockerfile.service - image for the backend service - will listen on 5000
@@ -14,3 +14,5 @@ a service created which can be run as multiple instances on different ports.
 4. Changes are made in both .py files to obtain the variables from environment rather the command line arguments.
 
 5. docker-compose up will spin up the instances for you. Load balancer can be tested at http://localhost:8080
+
+## v4 - Simple round robin loadbalancer with a healthcheck
--- loadbalancer/load_balancer.py
+++ loadbalancer/load_balancer.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 import logging
-import itertools
 import requests
 import os
+import time
+import threading
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -11,20 +12,67 @@ app = Flask(__name__)
 
 PORT = int(os.environ.get("PORT", 8080))
 BACKENDS = os.environ.get("BACKENDS", "").split(",")
-backend_cycle = itertools.cycle(BACKENDS)
+HEALTH_INTERVAL = int(os.environ.get("HEALTH_INTERVAL", 5))
 
-def get_next_backend():
-    return next(backend_cycle)
+class Backend:
+    def __init__(self, url):
+        self.url = url
+        self.healthy = True
+        self.last_check = None
+
+class LoadBalancer:
+    def __init__(self, backend_urls, health_check_interval=5):
+        self.backends = [Backend(url) for url in backend_urls]
+        self.health_check_interval = health_check_interval
+        self.current_index = 0
+        self.lock = threading.Lock()
+
+    def get_next_backend(self):
+        with self.lock:
+            healthy_backends = [b for b in self.backends if b.healthy]
+            if not healthy_backends:
+                return None
+            self.current_index = self.current_index % len(healthy_backends)
+            backend = healthy_backends[self.current_index]
+            self.current_index +=1
+            return backend
+
+    def check_health(self, backend):
+        try:
+            logger.info(f"health check for {backend.url}")
+            resp = requests.get(f"{backend.url}/health", timeout=2)
+            backend.healthy = resp.status_code == 200
+        except requests.exceptions.RequestException as e:
+            backend.healthy = False
+            
+        backend.last_check = time.time()
+        logger.info("backend : {backend.url} -> healthy: {backend.healthy}")
+
+    def health_check_loop(self):
+        while True:
+            for backend in self.backends:
+                self.check_health(backend)
+            time.sleep(self.health_check_interval)
+
+    def start_health_checks(self):
+        logger.info("health check loop started")
+        thread = threading.Thread(target=self.health_check_loop, daemon=True)
+        thread.start()
+    
+
+lb = LoadBalancer(BACKENDS, HEALTH_INTERVAL)
 
 @app.route("/")
 def proxy():
-    backend = get_next_backend()
+    backend = lb.get_next_backend()
     logging.info(f"forwarding request to {backend}")
 
+    if not backend:
+        return Response("no healthy backends", status=503)
     try:
         resp = requests.request(
             method = request.method,
-            url=backend,
+            url=backend.url,
             headers={k: v for k, v in request.headers if k.lower()!= "host"},    
             data=request.get_data(),
             allow_redirects=False
@@ -35,9 +83,14 @@ def proxy():
         logger.error(f"Backend {backend} failed: {e}")
         return Response("backend unavailable", status=502)
 
-    
+@app.route("/lb/status")
+def lbstatus():
+    return jsonify({
+        "backends": [{"url": b.url, "healthy": b.healthy, "last_check": b.last_check} for b in lb.backends]
+    })
+
 if __name__ == '__main__':
     logger.info(f"load balancer starting on port {PORT} ")
     logger.info(f"list of backends {BACKENDS}")
-    
+    lb.start_health_checks()
     app.run("0.0.0.0", port=PORT)
